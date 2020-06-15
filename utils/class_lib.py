27af4c715908f79418a49b553da6c9eab7026f13
mathilde.lapoix <mathilde.lapoix@gmail.com>
2020-06-15 13:54:18 +0200

    corrected for exceptions

--- utils/class_lib.py
+++ utils/class_lib.py
@@ -7,22 +7,30 @@ class Exp:
 
     def __init__(self, path, fishlabel, csv_path):
         # date of experiment
-        self.date = datetime.datetime.fromtimestamp(os.path.getmtime(path))
+        try:
+            self.date = datetime.datetime.fromtimestamp(os.path.getmtime(path))
+        except FileNotFoundError:
+            self.date = fishlabel.split('_')[0]
+
+        print('date fo experiment:', self.date)
+
+        # get parameters of experiment, either in the csv summary file or by user
+        csv_file = pd.read_csv(csv_path)
+        fishinfo = csv_file[csv_file['Fishlabel'] == fishlabel]
 
         # number of planes taken for this fish
         nPlanes = 0
-        for i in os.listdir(path):
-            if os.path.isdir(os.path.join(path, i)):
-                nPlanes += 1
+        try:
+            for i in os.listdir(path):
+                if os.path.isdir(os.path.join(path, i)):
+                    nPlanes += 1
+        except FileNotFoundError:
+            nPlanes = int(fishinfo['nPlanes'])
         print('Nplanes is ', nPlanes)
         a = str(input('Ok ? press anything for no'))
         if a:
             nPlanes = str(input('enter NPlanes:'))
         self.nPlanes = nPlanes
-
-        # get parameters of experiment, either in the csv summary file or by user
-        csv_file = pd.read_csv(csv_path)
-        fishinfo = csv_file[csv_file['Fishlabel'] == fishlabel]
 
         # frame rate of behavior acquisition
         fps_beh = fishinfo['FrameRateCamera']
