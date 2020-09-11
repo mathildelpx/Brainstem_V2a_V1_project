f2b6a44fbf68e785446485ea8f8c3dfd6a0c9cbf
mathilde.lapoix <mathilde.lapoix@gmail.com>
2020-09-11 14:32:37 +0200

    updated pickle load of classification output_dataframe

--- utils/load_classified_data.py
+++ utils/load_classified_data.py
@@ -2,9 +2,7 @@ import pickle
 import pandas as pd
 
 
-def load_output_dataframe(path, trial):
-    with open(path, 'rb') as f:
-        df = pickle.load(f)
-    output = df[df['Trial_ID'] == trial]
+def load_output_dataframe(path, trial_id):
+    df = pd.read_csv(path)
+    output = df[df['Trial_ID'] == trial_id]
     return output
-
