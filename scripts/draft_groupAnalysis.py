f136730b5ef9135cee7b724e4cb99af66849d7e4
mathilde.lapoix <mathilde.lapoix@gmail.com>
2020-12-09 12:33:05 +0100

    creation: put cells in group (based on position) and analyse group response

--- scripts/draft_groupAnalysis.py
+++ scripts/draft_groupAnalysis.py
@@ -3,9 +3,15 @@ import pandas as pd
 import numpy as np
 import matplotlib.pyplot as plt
 import logging
+import seaborn as sns
 
 from utils.functions_calcium_analysis import *
 from utils.plotting import *
+
+
+def find_indices(lst, condition):
+    return [i for i, elem in enumerate(lst) if condition(elem)]
+
 
 fishlabel = '200813_F1'
 depth = '160um'
@@ -39,8 +45,8 @@ bulbar_lat = []
 bulbar_medial = []
 pontine = []
 
-cells_group = [np.nan]*len(struct['iscell'])
-side = [np.nan]*len(struct['iscell'])
+cells_group = [np.nan] * len(struct['iscell'])
+side = [np.nan] * len(struct['iscell'])
 
 for cell in cells:
     if (struct['stat'][cell]['med'][0] <= bulbar_lat_lims[0]) or (struct['stat'][cell]['med'][0] >= bulbar_lat_lims[1]):
@@ -58,7 +64,7 @@ for cell in cells:
     else:
         side[cell] = 'left'
 
-#TODO: add another group to separate bulbar medial rostral and caudal
+# TODO: add another group to separate bulbar medial rostral and caudal
 
 print('Number of pontine cells:', len(pontine))
 print('Number of bulbar medial cells', len(bulbar_medial))
@@ -84,8 +90,8 @@ for cell in cells:
 plt.title('Cells group for fish ' + fishlabel + ', depth:' + depth)
 plt.ylabel('LEFT / RIGHT')
 plt.xlabel('ROSTRAL / CAUDAL')
-plt.savefig('/Users/test/Google Drive/PhD/Data_analysis/cellGroupsAnalysis_HE/'+
-            fishlabel+'_'+depth+'_cellPoseGroup_median.png')
+plt.savefig('/network/lustre/iss01/wyart/analyses/mathilde.lapoix/HE_behavior_CI_analysis/' +
+            fishlabel + '_' + depth + '_cellPoseGroup_median.png')
 plt.show()
 
 plt.figure()
@@ -97,14 +103,12 @@ for cell in cells:
     heatmap_group[ypix, xpix] = colors_int[cells_group[cell]]
 plt.imshow(struct['ops']['meanImg'], cmap='Greys_r')
 plt.imshow(heatmap_group, alpha=0.85, cmap='tab10', vmax=4)
-plt.plot([pontine_bulbar_lim, pontine_bulbar_lim], [0, struct['ops']['meanImg'].shape[0]-1], '--')
+plt.plot([pontine_bulbar_lim, pontine_bulbar_lim], [0, struct['ops']['meanImg'].shape[0] - 1], '--')
 plt.title('Cells group for fish ' + fishlabel + ', depth:' + depth)
 plt.ylabel('LEFT / RIGHT')
 plt.xlabel('ROSTRAL / CAUDAL')
-plt.savefig('/Users/test/Google Drive/PhD/Data_analysis/cellGroupsAnalysis_HE/'+
-            fishlabel+'_'+depth+'_cellPoseGroup_mask.png')
-plt.show()
-
+plt.savefig('/network/lustre/iss01/wyart/analyses/mathilde.lapoix/HE_behavior_CI_analysis/' +
+            fishlabel + '_' + depth + '_cellPoseGroup_mask.png')
 plt.figure()
 heatmap_side = np.zeros((struct['ops']['Ly'], struct['ops']['Lx']))
 heatmap_side[:] = np.nan
@@ -118,13 +122,12 @@ for cell in cells:
         heatmap_side[ypix, xpix] = 1
 plt.imshow(struct['ops']['meanImg'], cmap='Greys_r')
 plt.imshow(heatmap_side, alpha=0.85, cmap='PiYG')
-plt.plot([0, struct['ops']['meanImg'].shape[1]-1], [midline]*2, '--')
+plt.plot([0, struct['ops']['meanImg'].shape[1] - 1], [midline] * 2, '--')
 plt.title('Cells side for fish ' + fishlabel + ', depth:' + depth)
 plt.ylabel('LEFT / RIGHT')
 plt.xlabel('ROSTRAL / CAUDAL')
-plt.savefig('/Users/test/Google Drive/PhD/Data_analysis/cellGroupsAnalysis_HE/'+
-            fishlabel+'_'+depth+'_cellPoseSide_mask.png')
-plt.show()
+plt.savefig('/network/lustre/iss01/wyart/analyses/mathilde.lapoix/HE_behavior_CI_analysis/' +
+            fishlabel + '_' + depth + '_cellPoseSide_mask.png')
 
 ###########################################################
 
@@ -135,7 +138,7 @@ nBouts = len(struct['df_bouts'])
 nFrames = struct['DFF'].shape[1]
 frame_rate = 4.4
 frame_rate_beh = 300
-time_indices = [frame*(1/frame_rate) for frame in range(nFrames)]
+time_indices = [frame * (1 / frame_rate) for frame in range(nFrames)]
 lim_sup = 0.2
 
 
@@ -173,7 +176,7 @@ df_lf = pd.DataFrame({'cell': np.repeat(cells, nBouts),
                       })
 
 
-#TODO: change function to find max start - 1 or 2 frames
+# TODO: change function to find max start - 1 or 2 frames
 def find_max_index(bout, roi, df_bouts, dff, frame_rate, time_indices, lim_sup):
     """Get the frame index at which the max_dff was found around a bout"""
     start = df_bouts['BoutStart_summed'].iloc[bout] / frame_rate
@@ -218,41 +221,88 @@ def calc_cell_max_frame_all_bout(roi, df_bouts, dff, frame_rate, time_indices, l
 
 # fill max DF/F filtered column
 maxs_filtered = pd.Series(cells).apply(calc_cell_max_all_bout,
-                                          args=(struct['df_bouts'], struct['DFF_filtered'],
-                                                frame_rate_beh, struct['time_indices'], lim_sup))
+                                       args=(struct['df_bouts'], struct['DFF_filtered'],
+                                             frame_rate_beh, struct['time_indices'], lim_sup))
 df_lf.at[:, 'max_dff_f'] = np.array(maxs_filtered).flatten()
 
-
 max_indices_f = pd.Series(cells).apply(calc_cell_max_frame_all_bout,
-                                          args=(struct['df_bouts'], struct['DFF_filtered'],
-                                                frame_rate_beh, struct['time_indices'], lim_sup))
-df_lf.at[:,'index_max_dff_f'] = np.array(max_indices_f).flatten()
+                                       args=(struct['df_bouts'], struct['DFF_filtered'],
+                                             frame_rate_beh, struct['time_indices'], lim_sup))
+df_lf.at[:, 'index_max_dff_f'] = np.array(max_indices_f).flatten()
+
 
 # Create DataFrame with signal
 
 
-def build_df_signal(roi, dff, dff_filtered, cells_group, cells, df_bouts, windows):
+def build_df_signal_bout(df_bouts, lim_sup, time_indices, frame_rate_beh):
+    df = pd.DataFrame(columns=['bout',
+                               'bout_type', 'bout_dur', 'n_osc', 'abs_Max_Bend_Amp', 'Integral_TA',
+                               'relative_time_point',
+                               'plane'],
+                      index=np.arange(nFrames))
+    for bout in np.arange(nBouts):
+        start = df_bouts['BoutStart_summed'].iloc[bout]/frame_rate_beh
+        end = df_bouts['BoutStart_summed'].iloc[bout]/frame_rate_beh
+        ca_indices_frame = find_indices(time_indices, lambda e: start < e < end + lim_sup)
+
+        ci_start = ca_indices_frame[0] - 2  # take 2 frames before the actual start
+        if len(ca_indices_frame) > 1:
+            ci_end = ca_indices_frame[-1]
+        else:
+            ci_end = ca_indices_frame[0]
+
+        df['bout'].iloc[ci_start:ci_end] = bout
+        df['bout_type'].iloc[ci_start:ci_end] = df_bouts['category'].iloc[bout]
+        df['bout_dur'].iloc[ci_start:ci_end] = df_bouts['Bout_Duration'].iloc[bout]
+        df['n_osc'].iloc[ci_start:ci_end] = df_bouts['Number_Osc'].iloc[bout]
+        df['abs_Max_Bend_Amp'].iloc[ci_start:ci_end] = df_bouts['abs_Max_Bend_Amp'].iloc[bout]
+        df['Integral_TA'].iloc[ci_start:ci_end] = df_bouts['Integral_TA'].iloc[bout]
+
+        # find closest frame to bout onset
+        possible_frame_start = [ca_indices_frame[0], ca_indices_frame[0]-1, ca_indices_frame[0]+1]
+        real_start = possible_frame_start[np.argmin([abs(i*frame_rate - start) for i in possible_frame_start])]
+        relative_time_points = np.arange(int(real_start-2*frame_rate), int(real_start+2*frame_rate))
+        df['relative_time_point'].iloc[relative_time_points] = np.arange(0, len(relative_time_points))
+    return df
+
+
+df_signal_bout = build_df_signal_bout(struct['df_bouts'], lim_sup, time_indices, frame_rate_beh)
+
+
+def build_df_signal(roi, dff, dff_filtered, cells_group, df_signal_bout):
     df = pd.DataFrame(columns=['cell', 'bout', 'time_point', 'dff', 'dff_f',
                                'bout_type', 'bout_dur', 'n_osc', 'abs_Max_Bend_Amp', 'Integral_TA',
+                               'relative_time_point',
                                'x_pos', 'y_pos', 'cluster', 'group', 'side',
                                'plane'],
-                     index=np.arange(nFrames))
-    df['cell'] = [roi]*nFrames
+                      index=np.arange(nFrames))
+    df['cell'] = [roi] * nFrames
     df['x_pos']: np.repeat(x_positions, nFrames)
     df['y_pos']: np.repeat(y_positions, nFrames)
     df['time_point'] = time_indices
     df['dff'] = dff[roi]
     df['dff_f'] = dff_filtered[roi]
-    df['group'] = [cells_group[roi]]*nFrames
-    df['side'] = [side[roi]]*nFrames
-    df['plane'] = [depth]*nFrames
-    inf, sup = windows
-    for bout in np.arange(nBouts):
-        start, end = df_bouts['BoutStart_summed'].iloc[bout], df_bouts['BoutStart_summed'].iloc[bout]
-        df['stim'][start:end] = 'stim'+str(stim)
-        df['bout_type'].iloc[start:end] = df_bouts['category'].iloc[bout]
-        df['bout_dur'].iloc[start:end] = df_bouts['BoutDuration'].iloc[bout]
-        df['n_osc'].iloc[start:end] = df_bouts['n_osc'].iloc[bout]
-        df['abs_Max_Bend_Amp'].iloc[start:end] = df_bouts['abs_Max_Bend_Amp'].iloc[bout]
-        df['Integral_TA'].iloc[start:end] = df_bouts['Integral_TA'].iloc[bout]
+    df['group'] = [cells_group[roi]] * nFrames
+    df['side'] = [side[roi]] * nFrames
+    df['plane'] = [depth] * nFrames
+
+    df['bout'] = df_signal_bout.bout
+    df['bout_type'] = df_signal_bout.bout_type
+    df['bout_dur'] = df_signal_bout.bout_dur
+    df['n_osc'] = df_signal_bout.n_osc
+    df['abs_Max_Bend_Amp'] = df_signal_bout.abs_Max_Bend_Amp
+    df['Integral_TA'] = df_signal_bout.Integral_TA
+    df['relative_time_point'] = df_signal_bout.relative_time_point
+
     return df
+
+
+dict_df_signal = {}
+for cell in cells:
+    dict_df_signal[str(cell)] = build_df_signal(cell, struct['DFF'], struct['DFF_filtered'],
+                                                cells_group, df_signal_bout)
+
+df_signal_final = pd.concat(dict_df_signal)
+
+sns.lineplot(data=df_signal_final[df_signal_final['bout_type'] == 'S'], x='relative_time_point', y='dff_f', hue='group')
+plt.plot([frame_rate]*2, [0, 2], '--', 'r')
